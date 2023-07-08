ee6b747145020b7d0a6873d8bce6f17b60652ef6
mattdevio <magreenberg@fullsail.edu>
2023-07-08 01:31:50 -0600

    style: run rustfmt

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:3000")
-        .expect("Unable to bind port 3000");
+    let listener = TcpListener::bind("127.0.0.1:3000").expect("Unable to bind port 3000");
     run(listener)?.await
 }
