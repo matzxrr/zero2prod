bd4c97f910ce4e87b0c8218e0e62db6e72ddca68
mattdevio <magreenberg@fullsail.edu>
2023-07-08 01:24:36 -0600

    feat: adding my first test

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    let listener = TcpListener::bind("127.0.0.1:3000")
+        .expect("Unable to bind port 3000");
+    run(listener)?.await
 }
